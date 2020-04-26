036a70f23e17ed57c25122d3a42f0c291b34428b
Nicolò Cordaro <37053588+Nicordaro@users.noreply.github.com>
2020-04-26 19:28:05 +0200

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -5,7 +5,10 @@ from PIL import Image
 import os
 import os.path
 import sys
+import numpy
 
+train_idx_dir = 'Caltech101/train.txt'
+test_idx_dir = 'Caltech101/test.txt'
 
 def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
@@ -18,8 +21,29 @@ class Caltech(VisionDataset):
     def __init__(self, root, split='train', transform=None, target_transform=None):
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
 
-        self.split = split # This defines the split you are going to use
-                           # (split files are called 'train.txt' and 'test.txt')
+        self.split = split  # This defines the split you are going to use
+        # (split files are called 'train.txt' and 'test.txt'
+
+        # dataset = self
+        # if split=='train':
+        #     ##read all idxs for the train
+        #     with open(train_idx_dir) as idx:
+        #         data = idx.read().splitlines()
+        #         numpy.random.shuffle(data)
+        #         train = data
+        #         lenght = len(train)
+        #         half = int(lenght / 2)
+        #         self.train_indexes = data[:half]  # split the indices for your train split
+        #         self.val_indexes = data[half:]
+
+        self.categories = sorted(os.listdir(root))
+        self.categories.remove('BACKGROUND_Google')
+        self.index = []
+        self.y = []
+        for(i,c) in enumerate(self.categories):
+            self.index.append(i)
+            self.y.append(c)
+
 
         '''
         - Here you should implement the logic for reading the splits files and accessing elements
@@ -35,14 +59,13 @@ class Caltech(VisionDataset):
         __getitem__ should access an element through its index
         Args:
             index (int): Index
-
         Returns:
             tuple: (sample, target) where target is class_index of the target class.
         '''
 
-        image, label = ... # Provide a way to access image and label via index
-                           # Image should be a PIL Image
-                           # label can be int
+        image, label = pil_loader(index), self.y[index] # Provide a way to access image and label via index
+        # Image should be a PIL Image
+        # label can be int
 
         # Applies preprocessing when accessing the image
         if self.transform is not None:
@@ -55,5 +78,5 @@ class Caltech(VisionDataset):
         The __len__ method returns the length of the dataset
         It is mandatory, as this is used by several other components
         '''
-        length = ... # Provide a way to get the length (number of elements) of the dataset
+        length = self.index
         return length

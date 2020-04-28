a1e55a91e2e1497649d138d6e79e985b7c4bd9fb
Nicolò Cordaro <37053588+Nicordaro@users.noreply.github.com>
2020-04-28 11:53:13 +0200

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -7,8 +7,8 @@ import os.path
 import sys
 import numpy
 
-train_idx_dir = 'Caltech101/train.txt'
-test_idx_dir = 'Caltech101/test.txt'
+train_idx_dir = 'train.txt'
+test_idx_dir = 'test.txt'
 
 
 def pil_loader(path):
@@ -25,75 +25,13 @@ class Caltech(VisionDataset):
         self.split = split  # This defines the split you are going to use
         # (split files are called 'train.txt' and 'test.txt'
 
+        self.db = []
 
+        for folder in os.listdir(path=root):
+            for img in os.listdir(root + "/" + folder):
+                if folder != 'BACKGROUND_Google':
+                    self.db.append(folder + "/" + img)
 
-        #self.categories = {i: train[i] for i in range(0, len(listOfStr))}
-        self.indexes = []
-
-
-
-        if split=='train':
-            ##read all idxs for the train
-            with open(train_idx_dir) as idx:
-                data = idx.read().splitlines()
-
-                self.imgs = []
-
-
-                #prendo righe con percorsi img
-                for i in data:
-                    #divido in categ e img relativa a categoria
-                    categ = i.split("/")[0]
-                    #scarto bg google
-                    if categ != 'BACKGROUND_Google':
-                        #lista di categorie con duplicati, è ordinata in modo da tenere traccia di categoria
-                        # in base a posizione
-                        self.imgs.append(categ)
-                self.categories = list(numpy.unique(self.imgs))
-
-            for ind, imag in enumerate(self.imgs):
-                    self.indexes.append(ind)
-
-        if split == 'test':
-            ##read all idxs for the train
-            with open(test_idx_dir) as idx:
-                data = idx.read().splitlines()
-
-                self.imgs = []
-
-                # prendo righe con percorsi img
-                for i in data:
-                    # divido in categ e img relativa a categoria
-                    categ = i.split("/")[0]
-                    # scarto bg google
-                    if categ != 'BACKGROUND_Google':
-                        # lista di categorie con duplicati, è ordinata in modo da tenere traccia di categoria
-                        # in base a posizione
-                        self.imgs.append(categ)
-                self.categories = list(numpy.unique(self.imgs))
-
-            for ind, imag in enumerate(self.imgs):
-                self.indexes.append(ind)
-
-
-
-        # self.categories = sorted(os.listdir('Caltech101/'))
-        # #self.categories.remove('BACKGROUND_Google')
-        #
-        # # self.index = []
-        # # self.y = []
-        # # for(i,c) in enumerate(self.categories):
-        # #    self.index.append(i)
-        # #    self.y.append(c)
-        # self.index = []
-        # self.y = []
-        # for (i, c) in enumerate(self.categories):
-        #     print(c)
-        #     n = len(os.listdir('Caltech101/'))
-        #     self.index.extend(range(1, n + 1))
-        #     self.y.extend(n * [i])
-        # print(self.y)
-        # print(self.index)
         '''
         - Here you should implement the logic for reading the splits files and accessing elements
         - If the RAM size allows it, it is faster to store all data in memory
@@ -112,11 +50,17 @@ class Caltech(VisionDataset):
             tuple: (sample, target) where target is class_index of the target class.
         '''
 
-        def getimg(idx):
-            lab = self.imgs[idx]
-            return lab
+        # def getimg(idx):
+        #     lab = self.imgs[idx]
+        #     return lab
 
-        image, label = pil_loader(index), getimg(index)  # Provide a way to access image and label via index
+        def getlabel(idx):
+            item = self.db[idx]
+            item = item.split('/')[0]
+            return item
+
+
+        image, label = pil_loader(self.db[index]), getlabel(index)  # Provide a way to access image and label via index
         # Image should be a PIL Image
         # label can be int
 
@@ -131,8 +75,12 @@ class Caltech(VisionDataset):
         The __len__ method returns the length of the dataset
         It is mandatory, as this is used by several other components
         '''
-        length = len(self.index)
+        length = len(self.db)
         return length
 
-Caltech('Caltech101').__init__('')
-img, lab = Caltech('Caltech').__getitem__(2)
+
+a = Caltech('Caltech101')
+
+print(a.db)
+print(a.__len__())
+print(a.__getitem__(2))
